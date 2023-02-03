9e30247d34bfe50b035ee479b74b957e68448038
José Luís Ramos <joseluisramost3@gmail.com>
2023-02-03 20:16:12 -0300

    Update icon.py

--- homeassistant/helpers/icon.py
+++ homeassistant/helpers/icon.py
@@ -20,12 +20,7 @@ def icon_for_battery_level(
     return icon
 
 
-def icon_for_signal_level(signal_level: int | None = None) -> str:
-    """Return a signal icon valid identifier."""
+def icon_for_signal_level(signal_level: Optional[int] = None) -> str:
     if signal_level is None or signal_level == 0:
         return "mdi:signal-cellular-outline"
-    if signal_level > 70:
-        return "mdi:signal-cellular-3"
-    if signal_level > 30:
-        return "mdi:signal-cellular-2"
-    return "mdi:signal-cellular-1"
+    return "mdi:signal-cellular-3" if signal_level > 70 else "mdi:signal-cellular-2" if signal_level > 30 else "mdi:signal-cellular-1"
